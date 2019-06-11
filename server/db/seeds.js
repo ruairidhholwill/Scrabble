97eb3344cac68fa1403ecb3120fcc8d11810f53e
Ruairidh Holwill <roore@hotmail.co.uk>
2019-06-11 15:15:36 +0100

    seeds created and tile API ready

---
+++ server/db/seeds.js
@@ -0,0 +1,396 @@
+use scrabble
+
+db.dropDatabase()
+
+db.tiles.insertMany([{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'a',
+    value: 1
+}, 
+{
+    letter: 'b',
+    value: 3
+},  
+{
+    letter: 'b',
+    value: 3
+}, 
+{
+    letter: 'c',
+    value: 3
+},  
+{
+    letter: 'c',
+    value: 3
+}, 
+{
+    letter: 'd',
+    value: 2
+}, 
+{
+    letter: 'd',
+    value: 2
+}, 
+{
+    letter: 'd',
+    value: 2
+}, 
+{
+    letter: 'd',
+    value: 2
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'e',
+    value: 1
+}, 
+{
+    letter: 'f',
+    value: 4
+}, 
+{
+    letter: 'f',
+    value: 4
+}, 
+{
+    letter: 'g',
+    value: 2
+}, 
+{
+    letter: 'g',
+    value: 2
+}, 
+{
+    letter: 'g',
+    value: 2
+}, 
+{
+    letter: 'h',
+    value: 4
+}, 
+{
+    letter: 'h',
+    value: 4
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'i',
+    value: 1
+}, 
+{
+    letter: 'j',
+    value: 8
+}, 
+{
+    letter: 'k',
+    value: 5
+}, 
+{
+    letter: 'l',
+    value: 1
+}, 
+{
+    letter: 'l',
+    value: 1
+}, 
+{
+    letter: 'l',
+    value: 1
+}, 
+{
+    letter: 'l',
+    value: 1
+}, 
+{
+    letter: 'm',
+    value: 3
+}, 
+{
+    letter: 'm',
+    value: 3
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'n',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'o',
+    value: 1
+}, 
+{
+    letter: 'p',
+    value: 3
+}, 
+{
+    letter: 'p',
+    value: 3
+}, 
+{
+    letter: 'q',
+    value: 10
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 'r',
+    value: 1
+}, 
+{
+    letter: 's',
+    value: 1
+}, 
+{
+    letter: 's',
+    value: 1
+}, 
+{
+    letter: 's',
+    value: 1
+}, 
+{
+    letter: 's',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 't',
+    value: 1
+}, 
+{
+    letter: 'u',
+    value: 1
+}, 
+{
+    letter: 'u',
+    value: 1
+}, 
+{
+    letter: 'u',
+    value: 1
+}, 
+{
+    letter: 'u',
+    value: 1
+}, 
+{
+    letter: 'v',
+    value: 4
+}, 
+{
+    letter: 'v',
+    value: 4
+}, 
+{
+    letter: 'w',
+    value: 4
+}, 
+{
+    letter: 'w',
+    value: 4
+}, 
+{
+    letter: 'x',
+    value: 8
+}, 
+{
+    letter: 'y',
+    value: 4
+}, 
+{
+    letter: 'y',
+    value: 4
+}, 
+{
+    letter: 'z',
+    value: 10
+}])
